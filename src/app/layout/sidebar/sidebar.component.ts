bb331b201f45c78c1e9a20ddae270249891c219f
aman-nadaf7 <aman@1.com>
2025-12-15 15:18:27 +0530

    final upate

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -7,15 +7,10 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <aside class="sidebar">
-      <div class="sidebar__logo"><img src="/header/upswinglogo.png" alt="aura logo" height="31px" width="30px" class="last" class="select-input"></div>
-      <nav class="sidebar__nav">
-<button class="sidebar__item"><img src="/header/home.png" alt="aura logo" height="31px" width="30px" class="last" class="select-input"></button>        <button class="sidebar__item"><img src="/header/setting.png" alt="aura logo" height="31px" width="30px" class="last" class="select-input"></button>
-        <button class="sidebar__item"><img src="/header/watch.png" alt="aura logo" height="31px" width="30px" class="last" class="select-input"></button>
-        
-      </nav>
-      <div class="sidebar__user">
-        <img src="/header/profile.png" alt="aura logo" height="31px" width="30px" class="last" class="select-input">
-      </div>
+<nav class="sidebar__nav">
+<button class="sidebar__item"><img src="/header/wholesidebar.png" alt="aura logo" height="810px" width="66px" class="last" class="select-input">
+</button>
+</nav>
     </aside>
   `,
   styleUrls: ['./sidebar.component.scss']
--- src/app/shared/components/kpi-card/kpi-card.component.ts
+++ src/app/shared/components/kpi-card/kpi-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kpi-card',
   standalone: true,
-  imports: [CommonModule],
+  imports:[CommonModule],
   templateUrl: './kpi-card.component.html',
-  styleUrls: ['./kpi-card.component.scss']
+  styleUrls: ['./kpi-card.component.scss'],
 })
 export class KpiCardComponent {
-  @Input() label = '';
-  @Input() value = '';
-  @Input() subLabel = '';
+  @Input() label!: string;
+  @Input() value!: string;
+  @Input() subLabel: string | null = null;
+
+  @Input() delta: number | null = null;        // for Arrival / Departure
+  @Input() avgService: number | null = null;   // for Guest Request
+
+  get deltaText(): string | null {
+    if (this.delta == null) return null;
+    const prefix = this.delta >= 0 ? '+' : '';
+    return `${prefix}${this.delta.toFixed(1)}%`;
+  }
 }
